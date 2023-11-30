4da7bef3375525f679611ea982e6971a3ffbdb93
Igor Shadurin <igor.shadurin@gmail.com>
2023-11-30 22:35:25 +0300

    refactor: tests

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -1,15 +1,104 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
-
 contract Game {
-    function verify(uint8 byteData, uint256 gameId, bytes memory signature) external view returns (bool) {
-        bytes32 hash = keccak256(abi.encodePacked(byteData, gameId));
-        bytes32 message = MessageHashUtils.toEthSignedMessageHash(hash);
-        require(ECDSA.recover(message, signature) == msg.sender, "Invalid signature");
+    struct GameData {
+        address creator;
+        bytes32 secretHash;
+        uint256 creationTime;
+        uint256 betAmount;
+        address opponent;
+        uint8 opponentBet;
+        bool ended;
+    }
 
-        return true;
+    uint256 public feePercentage;
+    uint256 public revealSeconds;
+    uint256 public returnSeconds;
+    mapping(uint256 => GameData) public games;
+
+    event GameCreated(uint256 indexed gameId, address indexed creator);
+    event GameEnded(uint256 indexed gameId, address indexed winner);
+
+    constructor(uint256 _feePercentage, uint256 _revealSeconds, uint256 _returnSeconds) {
+        feePercentage = _feePercentage;
+        revealSeconds = _revealSeconds;
+        returnSeconds = _returnSeconds;
+    }
+
+    function create(uint256 gameId, bytes32 secret) external payable {
+        require(msg.value > 0, "Bet amount must be greater than 0");
+        require(games[gameId].creator == address(0), "Game already exists");
+
+        uint256 betAmount = msg.value - calculateFee(msg.value);
+        games[gameId] = GameData(msg.sender, secret, block.timestamp, betAmount, address(0), 0, false);
+
+        emit GameCreated(gameId, msg.sender);
+    }
+
+    function bet(uint256 gameId, uint8 betValue) external payable {
+        require(betValue >= 1 && betValue <= 3, "Bet must be between 1 and 3");
+        GameData storage game = games[gameId];
+        require(game.creator != address(0) && !game.ended, "Game does not exist or has ended");
+        require(game.creator != msg.sender, "Creator cannot bet on their own game");
+        require(game.opponent == address(0), "Game already has an opponent");
+
+        uint256 expectedBetAmount = game.betAmount;
+        require(msg.value == expectedBetAmount, "Incorrect bet amount");
+
+        game.opponent = msg.sender;
+        game.opponentBet = betValue;
+    }
+
+    function reveal(uint256 gameId, uint8 betValue, bytes32 secret) external {
+        GameData storage game = games[gameId];
+        require(game.creator == msg.sender, "Only creator can reveal");
+        require(!game.ended, "Game already ended");
+        require(block.timestamp <= game.creationTime + revealSeconds, "Reveal period expired");
+
+        bytes32 calculatedHash = keccak256(abi.encodePacked(gameId, betValue, secret));
+        require(calculatedHash == game.secretHash, "Invalid secret data");
+
+        game.ended = true;
+        address winner = determineWinner(game.creator, game.opponent, betValue, game.opponentBet);
+
+        if (winner == address(0)) {
+            // In case of a draw, return the betAmount to both players
+            payable(game.creator).transfer(game.betAmount);
+            payable(game.opponent).transfer(game.betAmount);
+        } else {
+            // Transfer the total bet amount to the winner
+            uint256 totalBetAmount = game.betAmount * 2;
+            payable(winner).transfer(totalBetAmount);
+        }
+
+        emit GameEnded(gameId, winner);
+    }
+
+    function returnStack(uint256 gameId) external {
+        GameData storage game = games[gameId];
+        require(game.creator == msg.sender, "Only creator can return stack");
+        require(game.opponent == address(0), "Game already has an opponent");
+        require(block.timestamp >= game.creationTime + returnSeconds, "Return period not reached");
+
+        game.ended = true;
+        uint256 totalAmount = game.betAmount + calculateFee(game.betAmount);
+        payable(msg.sender).transfer(totalAmount);
+    }
+
+    function calculateFee(uint256 amount) internal view returns (uint256) {
+        return amount * feePercentage / 100;
+    }
+
+    function determineWinner(address creator, address opponent, uint8 creatorBet, uint8 opponentBet) internal pure returns (address) {
+        if ((creatorBet == 1 && opponentBet == 2) ||
+        (creatorBet == 2 && opponentBet == 3) ||
+            (creatorBet == 3 && opponentBet == 1)) {
+            return opponent;
+        } else if (creatorBet == opponentBet) {
+            return address(0); // Draw, no winner
+        } else {
+            return creator;
+        }
     }
 }
--- test/Game/Game.Basic.test.ts
+++ test/Game/Game.Basic.test.ts
@@ -1,50 +1,156 @@
-import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
-import { expect } from 'chai';
-import { ethers } from 'hardhat';
+import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers'
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
 
 describe('Game', function () {
+  const feePercentage = 5
+  const revealSeconds = 300 // 5 minutes
+  const returnSeconds = 600 // 10 minutes
+
   async function deployGameFixture() {
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, otherAccount] = await ethers.getSigners()
 
-    const Game = await ethers.getContractFactory('Game');
-    const game = await Game.deploy();
+    const Game = await ethers.getContractFactory('Game')
+    const game = await Game.deploy(feePercentage, revealSeconds, returnSeconds)
 
-    return { game, owner, otherAccount };
+    return {game, owner, otherAccount}
   }
 
-  describe('verify', function () {
-    it('should successfully verify a valid signature', async function () {
-      const { game, owner } = await loadFixture(deployGameFixture);
+  describe('create', function () {
+    it('should allow a user to create a game', async function () {
+      const {game, owner} = await loadFixture(deployGameFixture)
+      const gameId = ethers.id('game1')
+      const secretHash = ethers.keccak256(ethers.toUtf8Bytes('secret'))
+      const tx = await game.connect(owner).create(gameId, secretHash, {value: ethers.parseEther('1')})
+      await expect(tx).to.emit(game, 'GameCreated').withArgs(gameId, owner.address)
+    })
+  })
 
-      const byteData = 2;
-      const gameId = 11;
-      const dataHash = ethers.solidityPackedKeccak256(['uint8', 'uint256'], [byteData, gameId]);
-      const messageBytes = Buffer.from(dataHash.slice(2), 'hex');
-      const signature = await owner.signMessage(messageBytes);
+  describe('bet', function () {
+    it('should not allow a user to place a bet on a non-existent game', async function () {
+      const {game, otherAccount} = await loadFixture(deployGameFixture)
+      const gameId = ethers.id('game1')
+      const betValue = 2
+      const betAmount = ethers.parseEther('1')
 
-      await expect(game.verify(byteData, gameId, signature)).to.not.be.reverted;
-    });
+      await expect(game.connect(otherAccount).bet(gameId, betValue, {value: betAmount}))
+          .to.be.revertedWith('Game does not exist or has ended')
+    })
+  })
 
-    it('should reject an invalid signature', async function () {
-      const { game, owner, otherAccount } = await loadFixture(deployGameFixture);
+  describe('reveal', function () {
+    it('should allow the game creator to reveal the game, nobody wins', async function () {
+      const {game, owner, otherAccount} = await loadFixture(deployGameFixture)
+      const gameId = ethers.id('game1')
+      const betValue = 2
+      const secret = ethers.encodeBytes32String('secret')
+      // const secretHash = ethers.keccak256(secret)
 
-      const byteData = 2;
-      const gameId = 11;
-      const dataHash = ethers.solidityPackedKeccak256(['uint8', 'uint256'], [byteData, gameId]);
-      const signature = await otherAccount.signMessage(dataHash);
+      const dataHash = ethers.solidityPackedKeccak256(['uint256', 'uint8', 'bytes32'], [gameId, betValue, secret])
+      const secretHash = Buffer.from(dataHash.slice(2), 'hex')
 
-      await expect(game.verify(byteData, gameId, signature)).to.be.revertedWith('Invalid signature');
-    });
+      // Create the game
+      await game.connect(owner).create(gameId, secretHash, {value: ethers.parseEther('1')})
 
-    it('should reject a signature from a different account', async function () {
-      const { game, owner, otherAccount } = await loadFixture(deployGameFixture);
+      const gameData = await game.games(gameId)
+      const betAmount = gameData[3]
 
-      const byteData = 2;
-      const gameId = 11;
-      const dataHash = ethers.solidityPackedKeccak256(['uint8', 'uint256'], [byteData, gameId]);
-      const signature = await owner.signMessage(dataHash);
+      // Other player places a bet
+      await game.connect(otherAccount).bet(gameId, betValue, {value: betAmount})
 
-      await expect(game.connect(otherAccount).verify(byteData, gameId, signature)).to.be.revertedWith('Invalid signature');
-    });
-  });
-});
+      // Skip time to allow reveal
+      await ethers.provider.send('evm_increaseTime', [revealSeconds - 10])
+      await ethers.provider.send('evm_mine')
+
+      // Reveal the game
+      await expect(game.connect(owner).reveal(gameId, betValue, secret))
+          .to.emit(game, 'GameEnded')
+          .withArgs(gameId, '0x0000000000000000000000000000000000000000')
+    })
+
+    it('should allow the game creator to reveal the game, second player wins', async function () {
+      const {game, owner, otherAccount} = await loadFixture(deployGameFixture)
+      const gameId = ethers.id('game1')
+      const creatorBetValue = 1
+      const opponentBetValue = 2
+      const secret = ethers.encodeBytes32String('secret')
+
+      const user2InitialBalance = await ethers.provider.getBalance(otherAccount.address)
+
+      const dataHash = ethers.solidityPackedKeccak256(['uint256', 'uint8', 'bytes32'], [gameId, creatorBetValue, secret])
+      const secretHash = Buffer.from(dataHash.slice(2), 'hex')
+
+      // Create the game with the creator's bet
+      await game.connect(owner).create(gameId, secretHash, {value: ethers.parseEther('1')})
+
+      // Get the bet amount from the created game
+      const gameData = await game.games(gameId)
+      const betAmount = gameData[3]
+
+      // Other player places a bet
+      await game.connect(otherAccount).bet(gameId, opponentBetValue, {value: betAmount})
+
+      // Skip time to allow reveal
+      await ethers.provider.send('evm_increaseTime', [revealSeconds - 10])
+      await ethers.provider.send('evm_mine')
+
+      // Get initial balance of the opponent
+      const initialBalance = await ethers.provider.getBalance(otherAccount.address)
+
+      // Reveal the game
+      const tx = await game.connect(owner).reveal(gameId, creatorBetValue, secret)
+
+      // Get updated balance of the opponent
+      const updatedBalance = await ethers.provider.getBalance(otherAccount.address)
+      expect(updatedBalance).to.be.greaterThan(user2InitialBalance)
+
+      // Check the emitted event for the correct winner
+      await expect(tx)
+          .to.emit(game, 'GameEnded')
+          .withArgs(gameId, otherAccount.address)
+    })
+
+    it('should allow the game creator to reveal the game, first player wins', async function () {
+      const {game, owner, otherAccount} = await loadFixture(deployGameFixture)
+      const gameId = ethers.id('game1')
+      const creatorBetValue = 2
+      const opponentBetValue = 1
+      const secret = ethers.encodeBytes32String('secret')
+
+      const user2InitialBalance = await ethers.provider.getBalance(otherAccount.address)
+
+      const dataHash = ethers.solidityPackedKeccak256(['uint256', 'uint8', 'bytes32'], [gameId, creatorBetValue, secret])
+      const secretHash = Buffer.from(dataHash.slice(2), 'hex')
+
+      // Create the game with the creator's bet
+      await game.connect(owner).create(gameId, secretHash, {value: ethers.parseEther('1')})
+
+      // Get the bet amount from the created game
+      const gameData = await game.games(gameId)
+      const betAmount = gameData[3]
+
+      // Other player places a bet
+      await game.connect(otherAccount).bet(gameId, opponentBetValue, {value: betAmount})
+
+      // Skip time to allow reveal
+      await ethers.provider.send('evm_increaseTime', [revealSeconds - 10])
+      await ethers.provider.send('evm_mine')
+
+      // Get initial balance of the opponent
+      const initialBalance = await ethers.provider.getBalance(otherAccount.address)
+
+      // Reveal the game
+      const tx = await game.connect(owner).reveal(gameId, creatorBetValue, secret)
+
+      // Get updated balance of the opponent
+      const updatedBalance = await ethers.provider.getBalance(otherAccount.address)
+      expect(updatedBalance).to.be.lessThan(user2InitialBalance)
+
+      // Check the emitted event for the correct winner
+      await expect(tx)
+          .to.emit(game, 'GameEnded')
+          .withArgs(gameId, owner.address)
+    })
+  })
+
+})
